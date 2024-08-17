7dc7c7385fab44295b7186c2ffa59821deccc0d7
Vilius Roškus <vro@danskebank.lt>
2024-08-17 10:45:21 +0300

    Update

--- eslint.config.mjs
+++ eslint.config.mjs
@@ -2,6 +2,9 @@ import eslint from '@eslint/js';
 import tseslint from 'typescript-eslint';
 import problems from 'eslint-config-problems';
 import perfectionist from 'eslint-plugin-perfectionist';
+import importPlugin from 'eslint-plugin-import';
+import importNewlinesPlugin from 'eslint-plugin-import-newlines';
+import reactPlugin from 'eslint-plugin-react';
 
 export default tseslint.config(
   eslint.configs.recommended,
@@ -10,4 +13,11 @@ export default tseslint.config(
     rules: problems.rules,
   },
   perfectionist.configs['recommended-natural'],
+  {
+    plugins: {
+      'import-newlines': importNewlinesPlugin,
+      importPlugin,
+      reactPlugin,
+    },
+  }
 );
